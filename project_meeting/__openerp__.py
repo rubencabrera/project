6b99b3d0b530475a27b469626d3a5476ba752bd8
Rubén Cabrera <rcabrera@praxya.com>
2017-04-07 14:18:45 +0200

    Module definition

---
+++ project_meeting/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rubén Cabrera Martínez, Praxya
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Project meeting",
+    "summary": "Gathers data with a controller for task and meeting report",
+    "version": "8.0.0.0.1",
+    "category": "Project",
+    "website": "www.praxya.com",
+    "author": "Rubén Cabrera Martínez, Praxya, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "project",
+    ],
+    # "data": [
+        # "security/some_model_security.xml",
+        # "security/ir.model.access.csv",
+        # "templates/assets.xml",
+        # "views/report_name.xml",
+        # "views/res_partner_view.xml",
+        # "wizards/wizard_model_view.xml",
+    # ],
+    # "demo": [
+        # "demo/res_partner_demo.xml",
+    # ],
+    # "qweb": [
+        # "static/src/xml/module_name.xml",
+    # ]
+}
