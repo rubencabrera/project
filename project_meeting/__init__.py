2bb8d6484cbe161200f9a8e1d50bb188ae177898
Rubén Cabrera <rcabrera@praxya.com>
2017-04-07 12:22:39 +0200

    Initial commit for project_meeting

---
+++ project_meeting/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rubén Cabrera Martínez, Praxya
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
+from . import wizards
+from . import controllers
+from . import report
